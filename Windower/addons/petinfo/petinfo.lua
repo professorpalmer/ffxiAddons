c2d0f4d339508ce9ba763794580d89eff0df0531
Cary Palmer <professorpalmer9@gmail.com>
2025-07-22 12:21:41 -0500

    Update and rename petinfo.lua to Windower/addons/petinfo/petinfo.lua

--- Windower/addons/petinfo/petinfo.lua
+++ Windower/addons/petinfo/petinfo.lua
@@ -1,23 +1,6 @@
---[[
-* Addons - Copyright (c) 2021 Ashita Development Team
-* Contact: https://www.ashitaxi.com/
-* Contact: https://discord.gg/Ashita
-*
-* This file is part of Ashita.
-*
-* Ashita is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* Ashita is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Ashita.  If not, see <https://www.gnu.org/licenses/>.
---]]
+-- PetInfo - Displays pet information in a GUI style display
+-- Palmer (Zodiarchy @ Asura)
+-- 2025-07-22
 
 config = require('config')
 texts = require('texts')
@@ -25,7 +8,7 @@ packets = require('packets')
 require('strings')
 
 _addon.name = 'PetInfo'
-_addon.author = 'Converted from Ashita by Palmer'
+_addon.author = 'Palmer (Zodiarchy @ Asura)'
 _addon.version = '1.0'
 _addon.commands = {'petinfo', 'pi'}
 
